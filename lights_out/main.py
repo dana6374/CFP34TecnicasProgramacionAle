fd25538a4ce85c09ccec69528b2090ceab99cf61
dana6374 <alesolecaffa@yahoo.com>
2016-10-26 20:06:20 -0300

    Progrma Lights out  Menu. Muestro menu principal

---
+++ lights_out/main.py
@@ -0,0 +1,10 @@
+import menu
+print(" ")
+print("Binevenido al juego LIGHTS OUT")
+print(" ")
+modo = input("Elija el modo de juego (1 = Aleatorio, 2 = Nivel 1): ")
+
+menu.mostrarMenu(modo)
+
+
+
---
+++ lights_out/menu.py
@@ -0,0 +1,6 @@
+def mostrarMenu(modo):
+
+    if modo==1:
+        print("Aleatorio")
+    else:
+         print("Nivel 1")
