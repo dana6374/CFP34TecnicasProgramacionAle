97f332bd03bd3d4d291ae587b853e3d53697e989
dana6374 <alesolecaffa@yahoo.com>
2016-11-09 19:41:00 -0300

    Programa

--- lights_out/conversorCoordenadas.py
+++ lights_out/conversorCoordenadas.py
@@ -3,4 +3,5 @@
 def convertirCoordenadas(coordenadas):
     diccionarioCoordenadas = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
     #todo hacerloo
+
     return coordenadas
--- lights_out/juegoPredeterminado.py
+++ lights_out/juegoPredeterminado.py
@@ -5,3 +5,4 @@ def jugar():
     print("Jugando en modo predeterminado !")
     coordenadasDelUsuario = coordenadas.solicitarCoordenadas()
     print("LA COORDENADA FUE VALIDA !!!! ",coordenadasDelUsuario)
+    #todo juego
