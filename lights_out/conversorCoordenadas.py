48da2e852e2424ab5d8604bc07a81fd4303520d1
dana6374 <alesolecaffa@yahoo.com>
2016-11-09 20:07:19 -0300

    Programa

--- lights_out/conversorCoordenadas.py
+++ lights_out/conversorCoordenadas.py
@@ -1,13 +1,11 @@
-# El parámetro "coordenadas" que recibe ésta función es del tipo STRING y mide 2 Caracteres
+# Pre: El parámetro "coordenadas" que recibe ésta función es del tipo STRING y mide 2 Caracteres
+# Post: Cuando los datos no son válidos se devuelven indices negativos.
 
 def convertirCoordenadas(coordenadas):
     diccionarioColumnas = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
     diccionarioFilas= {"1": 0, "2": 1, "3": 2, "4": 3, "5": 4}
 
-    for coordenadas[1] in diccionarioColumnas:
-        for coordenadas[0] in diccionarioFilas:
-            print(diccionarioFilas, diccionarioColumnas)
-
-
-
-    return coordenadas
+    if coordenadas[1] in diccionarioColumnas and coordenadas[0] in diccionarioFilas:
+            return (diccionarioFilas, diccionarioColumnas)
+    else:
+        (-1,-1)
