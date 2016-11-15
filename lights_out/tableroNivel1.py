7c6dc813a3cd2e5c2347d3d7dc782a234908b53d
Maq301 <alesolecaffa@yahoo.com>
2016-11-15 19:59:12 -0300

    Creacion de niveles

---
+++ lights_out/tableroNivel1.py
@@ -0,0 +1,46 @@
+
+def imprimirTableroNivel1():
+    tableroNivel1 = [["o", "o", ".", "o", "o"],
+                     ["o", ".", "o", ".", "o"],
+                     [".", "o", "o", "o", "."],
+                     ["o", ".", "o", ".", "o"],
+                     ["o", "o", ".", "o", "o"]]
+    for elemento in tableroNivel1:
+        print(elemento)
+
+def imprimirTableroNivel2():
+    tableroNivel2 = [[".", "o", ".", "o", "."],
+                     ["o", "0", ".", ".", "o"],
+                     [".", "o", ".", "o", "."],
+                     ["o", ".", "0", ".", "o"],
+                     ["o", ".", "0", ".", "o"]]
+    for elemento in tableroNivel2:
+        print(elemento)
+
+def imprimirTableroNivel3():
+    tableroNivel3 = [["o", "o", ".", "o", "o"],
+                     ["o", ".", "o", ".", "o"],
+                     [".", "o", "o", "o", "."],
+                     ["o", ".", "o", ".", "o"],
+                     ["o", "o", ".", "o", "o"]]
+    for elemento in tableroNivel3:
+        print(elemento)
+
+def imprimirTableroNivel4():
+    tableroNivel4 = [["o", "o", ".", "o", "o"],
+                     ["o", ".", "o", ".", "o"],
+                     [".", "o", "o", "o", "."],
+                     ["o", ".", "o", ".", "o"],
+                     ["o", "o", ".", "o", "o"]]
+    for elemento in tableroNivel4:
+        print(elemento)
+
+def imprimirTableroNivel5():
+    tableroNivel5 = [["o", "o", ".", "o", "o"],
+                     ["o", ".", "o", ".", "o"],
+                     [".", "o", "o", "o", "."],
+                     ["o", ".", "o", ".", "o"],
+                     ["o", "o", ".", "o", "o"]]
+    for elemento in tableroNivel5:
+        print(elemento)
+
