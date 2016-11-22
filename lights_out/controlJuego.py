d8d564e11ea85e16d773de7a9ce45d67013a80b3
Maq301 <alesolecaffa@yahoo.com>
2016-11-22 18:51:24 -0300

    generación de los niveles

--- lights_out/controlJuego.py
+++ lights_out/controlJuego.py
@@ -1,4 +1,6 @@
 def contador():
  #TODO
-
+"""contador desde 15 movimientos a que se muestre cada vez que el jugador ingresa una coordenada
+Cuando gana sale del juego
+Si todas las casillas del tablero = a .(puntito) entonces ganó y exit"""
 
--- lights_out/juegoPredeterminado.py
+++ lights_out/juegoPredeterminado.py
@@ -4,8 +4,10 @@ import puntosTablero
 def jugar():
 
     print("Jugando en modo predeterminado !")
+
     coordenadasDelUsuario = coordenadas.solicitarCoordenadas()
-    print("LA COORDENADA FUE VALIDA !!!! ",coordenadasDelUsuario)
+    print("LA COORDENADA FUE VALIDA !!!! ", coordenadasDelUsuario)
     puntosTablero.generarPuntos(coordenadasDelUsuario)
+    """El jugador"""
 
     #todo juego
