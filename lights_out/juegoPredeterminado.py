e27aed5ba0d2c9e3947e4cbb6e0756dbf16da5e0
Maq301 <alesolecaffa@yahoo.com>
2016-12-13 18:28:39 -0300

    reasignación de puntaje por nivel

--- lights_out/juegoPredeterminado.py
+++ lights_out/juegoPredeterminado.py
@@ -19,29 +19,25 @@ print("")
 
 def jugar():
 
-
     miTablero = copy.deepcopy(tableroNiveles.llamarNivel[controlJuego.nivel])
-    # donde dice estructuraTablero1, el 1 es una variable para nivel. Ese nro tiene que ser una variable.
 
 
-    print ("Usted está en el nivel: ", controlJuego.nivel)
+    print ("Usted está ahora en el nivel: ", controlJuego.nivel)
     print("")
     while controlJuego.intentos > 0 and not controlJuego.nivelCompleto(miTablero):
 
         tablero.imprimirTablero(miTablero)
-
         coordenadasDelUsuario = coordenadas.solicitarCoordenadas()
+
         print ("")
         print ("La coordenada: ", coordenadasDelUsuario, "es valida")
         print ("")
 
         puntosACambiar= puntosTablero.generarPuntos(coordenadasDelUsuario, miTablero)
-
         puntosACambiar.append(coordenadasDelUsuario)
-
         cambiarPuntos(puntosACambiar, miTablero)
-
         controlJuego.intentos = controlJuego.intentos-1
+
         print("Quedan", controlJuego.intentos, "intentos")
         print("")
 
@@ -49,16 +45,22 @@ def jugar():
 
         print("nivel ganado")
         print("")
+
         controlJuego.puntaje[controlJuego.nivel] = controlJuego.puntaje[controlJuego.nivel] + 500
+
         print("Puntaje del nivel: ", controlJuego.puntaje[controlJuego.nivel])
         print("")
         print ("Ud finalizó el nivel:", controlJuego.nivel)
         print("")
+
         controlJuego.nivel = controlJuego.nivel + 1
+        controlJuego.intentos = 15
+        print("cantidad de intentos para éste nivel: ", controlJuego.intentos)
+
 
         jugar()
 
     else:
-        print("perdí")
+        print("juego perdido")
         controlJuego.puntaje[controlJuego.nivel] = controlJuego.puntaje[controlJuego.nivel] - 300
         print ("Puntaje final: ", controlJuego.puntaje[controlJuego.nivel])
