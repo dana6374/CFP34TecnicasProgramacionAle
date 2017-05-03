c4a188d8b19f7a0edcdf5dc8d8d890ac692d020a
dana6374 <alesolecaffa@yahoo.com>
2017-05-03 20:13:40 -0300

    base de datos Menu alta baja modificacion consulta

--- base de datos/consulta.py
+++ base de datos/consulta.py
@@ -1,7 +1,9 @@
 def ejec()
     opcion = (input(str("seleccione: 1 = Estructura de Base de datos, 2 = listado de fabricantes, 3= listado de Artículos")))
 
-if opcion == "1":
+if opcion == "2":
+    c.execute()
+if opcion == "2":
    c.execute("SELECT * FROM FABRICANTES")
-if opcion == "2"
+if opcion == "3":
     c.execute("SELECT * FROM ARTICULOS")
