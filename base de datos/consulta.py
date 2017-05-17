c58af8e4a8fb72e8f45e39ac0a38372641a44283
dana6374 <alesolecaffa@yahoo.com>
2017-05-17 19:29:25 -0300

    opcion "consulta" creación

--- base de datos/consulta.py
+++ base de datos/consulta.py
@@ -1,8 +1,12 @@
 def ejec()
-    opcion = (input(str("seleccione: 1 = Estructura de Base de datos, 2 = listado de fabricantes, 3= listado de Artículos")))
+    opcion = input(str("seleccione: 1 = Estructura de Base de datos, 2 = listado de fabricantes, 3= listado de Artículos"))
+
+if opcion == "1":
+    c.execute()
 
 if opcion == "2":
-    c.execute()
+    c.execute('''SELECT ARTICULOS.ID, ARTICULOS.NOMBRE, ARTICULOS.PRECIO, FABRICANTES.NOMBRE
+  FROM ARTICULOS INNER JOIN FABRICANTES ON FABRICANTES.ID = ARTICULOS.FAB''')
 if opcion == "2":
    c.execute("SELECT * FROM FABRICANTES")
 if opcion == "3":
