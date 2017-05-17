c12c7a51af364f1b992f3f9aa57bea1ffa2a0e12
dana6374 <alesolecaffa@yahoo.com>
2017-05-17 18:09:34 -0300

    Modificacion de controles base de datos

--- base de datos/mainBaseDatos.py
+++ base de datos/mainBaseDatos.py
@@ -1,9 +1,12 @@
 import sqlite3
 import menuBaseDatos
 
+base = sqlite3.connect('d:\pi.db')
+c = base.cursor()
+
+
 print(" ")
 print("ABMC BD")
 print(" ")
 
-
-menu.mostrarMenu()
+menuBaseDatos.mostrarMenu()
