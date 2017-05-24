45fb13c7063a80425e9866b82cedb75e648a7879
dana6374 <alesolecaffa@yahoo.com>
2017-05-24 18:14:32 -0300

    Modificacion codigo base de datos Uso de archivo externo para crear la base.

--- base de datos/leerarchivo.py
+++ base de datos/leerarchivo.py
@@ -1,7 +1,12 @@
+import sqlite3
+
+base = sqlite3.connect('pi2.db')
+cursorbase = base.cursor()
+
 infile =open('bd.sql', 'r')
 print('>>> Lectura del fichero línea a línea')
 
 for line in infile:
-    print(line)
+    cursorbase.execute(line)
 # Cerramos el fichero.
 infile.close()
