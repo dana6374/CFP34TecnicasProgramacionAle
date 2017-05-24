be6cb2df6b11c67c1909f1aee28d99646789088c
dana6374 <alesolecaffa@yahoo.com>
2017-05-24 18:54:54 -0300

    Modificacion codigo base de datos Uso de archivo externo para crear la base.

--- base de datos/Alta.py
+++ base de datos/Alta.py
@@ -1,3 +1,15 @@
-def ejec()
-    for line in open('bd.sql'):
-        c.execute(line)
+def AltaDato()
+    modo= input(str("1 = Fabricante, 2 = Articulos, 3 = Cliente, 5= Volver a la opción anterior"))
+
+    opc = {1: "FABRICANTES",
+           2: "ARTICULOS",
+           3: "CLIENTES"}
+
+    if modo == 1
+    c.execute('INSERT INTO FABRICANTES(NOMBRE) VALUES("{}");'.format(nombre))
+    base.commit()
+    #PUEDE SER QUE LA OPCION DEL USUARIOS SEA EN LA VARIABLE OPC?
+
+    if modo == 2
+    c.execute ('INSERT INTO (NOMBRE, PRECIO, FAB) VALUES ("{}", {}, (SELECT ID FROM FABRICANTES WHERE (NOMBRE = "{}")'.format(NOMBRE, PRECIO, FABRICANTE))
+    base.commit()
--- base de datos/leerarchivo.py
+++ base de datos/leerarchivo.py
@@ -4,7 +4,6 @@ base = sqlite3.connect('pi2.db')
 c = base.cursor()
 
 infile=open('bd.sql', 'r')
-print('>>> Lectura del fichero línea a línea')
 
 for line in infile:
     c.execute(line)
