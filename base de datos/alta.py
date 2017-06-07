e951b0111e465ccba35db232dbee375bcaffa291
Dana6374 <alesolecaffa@yahoo.com>
2017-06-07 20:17:03 -0300

    modificacion del modulo ALTA

--- base de datos/alta.py
+++ base de datos/alta.py
@@ -2,23 +2,21 @@
 import baseDatos
 
 def ejec():
-    modo= input(str("1 = Fabricante, 2 = Articulos, 3 = Cliente, 5= Volver a la opción anterior"))
+    modo= input(str("1 = Fabricante, 2 = Articulo y precio, 3 = Cliente, 5= Volver a la opción anterior"))
 
     opciones = {1: "FABRICANTES",
-           2: "ARTICULOS",
-           3: "CLIENTES"}
+                2: "ARTICULOS",
+                3: "CLIENTES"}
 
     if modo == "1":
-        nombre = 'Ale'
-        #TODO: pedirle al usuario que nombre quiere ponerle al fabricante
+        nombre = input(str("Ingrese el nombre del fabricante. Si existe, no se modifica "))
         baseDatos.getCursorDeBaseDedatos().execute('INSERT INTO FABRICANTES(NOMBRE) VALUES("{}");'.format(nombre))
         baseDatos.baseDeDatos.commit()
-    #PUEDE SER QUE LA OPCION DEL USUARIOS ESTÉ EN LA VARIABLE OPC?
 
     if modo == "2":
-        #TODO: pedirle al usuario que nombre quiere ponerle al articulo
-        #TODO: pedirle al usuario que precio quiere ponerle al articulo
-        #TODO: pedirle al usuario que  ID tiene al articulo
+        nombre = input(str("Ingrese el nombre, precio del Articulo. Si existe, no se modifica "))
+        baseDatos.getCursorDeBaseDedatos().execute('INSERT INTO ARTICULOS(NOMBRE) VALUES("{}");'.format(nombre))
+        baseDatos.baseDeDatos.commit()
 
         nombreArticulo = "prueba"
         precioArticulo = 10
